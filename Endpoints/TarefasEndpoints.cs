9410a41dbf7eab4881ef559d4385330d53d7f774
Luan Pinto <luan.nelmo@hotmail.com>
2022-08-14 22:37:25 -0300

    Utilizando Minimal API no ASP.NET com SQL Server - 4

--- Endpoints/TarefasEndpoints.cs
+++ Endpoints/TarefasEndpoints.cs
@@ -20,6 +20,40 @@ namespace TarefasAPISQL.Endpoints
                 }
                 return Results.Ok(tarefas);
             });
+
+            app.MapGet("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                return con.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound();
+            });
+
+            app.MapPost("/tarefas", async (GetConnection connectionGetter, Tarefa tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Insert(tarefa);
+                return Results.Created($"/tarefas/{id}", tarefa);
+            });
+
+            app.MapPut("/tarefas", async (GetConnection connectionGetter, Tarefa tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(tarefa);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+
+                var deleted = con.Get<Tarefa>(id);
+
+                if (deleted is null)
+                {
+                    return Results.NotFound();
+                }
+                con.Delete(deleted);
+                return Results.Ok(deleted);
+            });
         }
     }
 }
